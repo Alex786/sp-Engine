9e92b4ff0f14ee6fd89a79303b1b440b6572444d
Alexander Brown <32424872+Alex786@users.noreply.github.com>
2024-10-15 20:18:29 -0500

    First commit
    New

---
+++ include/sp.h
@@ -0,0 +1,24 @@
+﻿// sp.h : Include file for standard system include files,
+// or project specific include files.
+
+//#pragma once
+
+//#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+
+#ifndef MYCLASS_H
+#define MYCLASS_H
+class MyClass {
+public:
+	const char* nameOfApp = "sp Engine";
+	const char* verOfApp = "1.0.0";
+	const char* identityOfApp = "com.alexander_brown.sp_engine1";
+	const char* creatorOfApp = "Alexander Brown";
+	const char* copyrightOfApp = "Copyright (c) 2024 Alexander Brown";
+	const char* urlOfApp = "www.google.com";
+	const char* typeOfApp = "application";
+};
+
+#endif // MYCLASS_H
+
---
+++ main.cpp
@@ -0,0 +1,37 @@
+﻿// sp.cpp : Defines the entry point for the application.
+//
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3/SDL_init.h>
+#include "include/sp.h"
+
+int main(int argc, char* argv[]) {
+	// Set App Metadata
+	const char* nameOfApp = "sp Engine";
+	const char* verOfApp = "1.0.0";
+	const char* identityOfApp = "com.alexander_brown.sp_engine1";
+	const char* creatorOfApp = "Alexander Brown";
+	const char* copyrightOfApp = "Copyright (c) 2024 Alexander Brown";
+	const char* urlOfApp = "www.google.com";
+	const char* typeOfApp = "application";
+
+	
+
+	SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_NAME_STRING, nameOfApp);
+	SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_VERSION_STRING, verOfApp);
+	SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_IDENTIFIER_STRING, identityOfApp);
+	SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_CREATOR_STRING, nameOfApp);
+	SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_NAME_STRING, nameOfApp);
+	SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_NAME_STRING, nameOfApp);
+	SDL_SetAppMetadataProperty(SDL_PROP_APP_METADATA_NAME_STRING, nameOfApp);
+
+	// Init
+	SDL_Init(SDL_INIT_VIDEO);
+	char d[] = "Super long message about some stuff! This is an SDL3 Window message!";
+	int sdlver = SDL_VERSION;
+	const char* sdlvertext = "";
+	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "title", d, NULL);
+	SDL_Quit;
+	return 0;
+}
